0932b3f8e6b85220341e868510b095336f648167
Magnanimouse_RE <augustusflick@gmail.com>
2025-05-22 11:30:32 +0200

    modified:   app/next.config.ts

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,8 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
 
   // everything in your Next app
-  // will be served under /app
-  basePath: '/app',
 
   // make sure your static assets (_next/*) also live under /app
   assetPrefix: '/app',
